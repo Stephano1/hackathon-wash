3bbab76f30465b891af61fc22bd70a6ff413498d
Ostap <fantom2012@ukr.net>
2016-11-12 23:07:49 +0200

    geocoder fixed

--- js/scripts.js
+++ js/scripts.js
@@ -18,23 +18,51 @@ div.innerHTML = e;
 
 
 function toggleClass(element, className){
-    if (!element || !className){
-        return;
-    }
-    
-    var classString = element.className, nameIndex = classString.indexOf(className);
-    if (nameIndex == -1) {
-        classString += ' ' + className;
-    }
-    else {
-        classString = classString.substr(0, nameIndex) + classString.substr(nameIndex+className.length);
-    }
-    element.className = classString;
+  if (!element || !className){
+    return;
+  }
+
+  var classString = element.className, nameIndex = classString.indexOf(className);
+  if (nameIndex == -1) {
+    classString += ' ' + className;
+  }
+  else {
+    classString = classString.substr(0, nameIndex) + classString.substr(nameIndex+className.length);
+  }
+  element.className = classString;
 }
 
 document.getElementById('popup-btn').addEventListener('click', function() {
-    toggleClass(document.getElementById('popup-washer'), 'popup-add-wash-visible');
+  toggleClass(document.getElementById('popup-washer'), 'popup-add-wash-visible');
+  var map2 = new google.maps.Map(document.getElementById('map2'), {
+    zoom: 13,
+    scrollwheel: false,
+    center: {lat: 49.839683, lng: 24.029717}
+  });
+  var geocoder = new google.maps.Geocoder();
+    geocodeAddress(geocoder, map2);
+
+  document.getElementById('submit').addEventListener('click', function() {
+    geocodeAddress(geocoder, map2);
+  });
+function geocodeAddress(geocoder, resultsMap) {
+  var address = document.getElementById('address').value;
+  geocoder.geocode({'address': address}, function(results, status) {
+    if (status === google.maps.GeocoderStatus.OK) {
+      resultsMap.setCenter(results[0].geometry.location);
+      var marker = new google.maps.Marker({
+        map: resultsMap,
+        position: results[0].geometry.location
+      });
+    } else {
+      alert('Geocode was not successful for the following reason: ' + status);
+    }
+  });
+}
 });
+
+
+
 
 
 
