e49b002d38cad32dfe5f75d00780c59bffc4ddf2
Ostap <fantom2012@ukr.net>
2016-11-13 11:53:55 +0200

    validation

--- js/scripts.js
+++ js/scripts.js
@@ -43,39 +43,53 @@ document.getElementById('popup-btn').addEventListener('click', function() {
 
 
   var geocoder = new google.maps.Geocoder();
-    geocodeAddress(geocoder, map2);
+  geocodeAddress(geocoder, map2);
 
   document.getElementById('submit').addEventListener('click', function() {
     geocodeAddress(geocoder, map2);
   });
-function geocodeAddress(geocoder, resultsMap) {
-  var address = document.getElementById('address').value;
-  geocoder.geocode({'address': address}, function(results, status) {
-    if (status === google.maps.GeocoderStatus.OK) {
-      resultsMap.setCenter(results[0].geometry.location);
-      var marker = new google.maps.Marker({
-        map: resultsMap,
-        position: results[0].geometry.location,
-      });
-    } else {
-      alert('Geocode was not successful for the following reason: ' + status);
-    }
-  });
-}
+  function geocodeAddress(geocoder, resultsMap) {
+    var address = document.getElementById('address').value;
+    geocoder.geocode({'address': address}, function(results, status) {
+      if (status === google.maps.GeocoderStatus.OK) {
+        resultsMap.setCenter(results[0].geometry.location);
+        var marker = new google.maps.Marker({
+          map: resultsMap,
+          position: results[0].geometry.location,
+        });
+      } else {
+        alert('Geocode was not successful for the following reason: ' + status);
+      }
+    });
+  }
 // google.maps.event.addListener(map2, 'click', function(event){alert('Lat: ' + event.latLng.lat() + ' Lng: ' + event.latLng.lng())});
 });
 
 document.getElementById('remove-popup').addEventListener('click', function() {
   toggleClass(document.getElementById('popup-washer'), 'popup-add-wash-visible');
+});
 
 
+document.getElementById('thanks-popup').addEventListener('click', function() {
+  var inputVal = document.getElementById('datepicker').value;
+  var thanks = document.getElementById('thanks');
+  var text = thanks.appendChild(document.createElement('p'));
+  text.innerHTML = inputVal;
+
+  var inputdateVal = document.getElementById('datetime').value;
+  var thanks = document.getElementById('thanks');
+  var text = thanks.appendChild(document.createElement('p'));
+  text.innerHTML = inputdateVal;
+
+  toggleClass(document.getElementById('show-thanks'), 'popup-show-thanks-visible');
+
+});
 
 
-  });
-
-
-
-
+document.getElementById('close-thanks').addEventListener('click', function() {
+  toggleClass(document.getElementById('show-thanks'), 'popup-show-thanks-visible');
+  
+});
 
 // mailer
 
